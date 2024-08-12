2a992cc3ff848cbede281384b0831e435b3e7d0d
Eric King <erickingboston@gmail.com>
2024-08-12 14:51:19 -0400

    Create main.py
    inputing first day of code

---
+++ Day1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Generator!")
+city = input("what city did you grow up in?\n")
+pet = input("what is the name of your first pet?\n")
+print("Your band is " + city + " " + pet)
